a147702caca1b0f15c1dbb5fae239fb10df79d33
Alexander Sender <141014024+SenderAlex@users.noreply.github.com>
2025-03-30 10:03:38 +0300

    start

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import TelegramUser
+from .serializers import TelegramUserSerializer
 
-# Create your views here.
+
+@api_view(['POST'])
+def user_registration(request):
+    data = request.data
+    user, created = TelegramUser.objects.get_or_create(
+        user_id=data['user_id'],
+        defaults={
+            'username': data.get('username', '')
+        }
+    )
+    if created:
+        serializer = TelegramUserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response({'message': 'User has been already created'}, status=status.HTTP_409_CONFLICT)
