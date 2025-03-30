69c93e0a2b6575d4f3abb9e35badf537c97a535d
Alexander Sender <141014024+SenderAlex@users.noreply.github.com>
2025-03-30 21:04:59 +0300

    start

---
+++ bot_main.py
@@ -0,0 +1,33 @@
+#@freelancer23_bot
+import telebot
+from telebot.types import Message
+import requests
+
+
+API_URL = 'http://127.0.0.1:8000/api'
+BOT_TOKEN = '6547643857:AAELyo-54AsPdF1vMeB5a1JukAROO0lqgA4'
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_command(message: Message):
+    data = {
+        'user_id': message.from_user.id,
+        'username':message.from_user.username
+    }
+    response = requests.post(API_URL, json=data)  # почему post???
+    if not response.status_code == 200:
+        if response.json().get('message'):
+            bot.send_message(message.chat.id, 'Пользователь с таким именем уже зарегистрирован')
+        else:
+            bot.send_message(message.chat.id, f'Вы успешно зарегистрированы!! Ваш уникальный номер '
+                                              f'{response.json()['id']}')
+    else:
+        bot.send_message(message.chat.id, 'Произошла ошибка при регистрации!!!')
+        print(response.json())
+        print(response.status_code)
+        print(response.text)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
