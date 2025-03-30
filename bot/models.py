3ddb7f20cfd3acc4ca7df177fba48fcf957e4b30
Alexander Sender <141014024+SenderAlex@users.noreply.github.com>
2025-03-30 09:04:40 +0300

    start

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class TelegramUser(models.Model):
+    user_id = models.BigIntegerField(unique=True)  # ID telegram
+    username = models.CharField(max_length=255, blank=True, null=True)  # username
+    created_at = models.DateTimeField(auto_now_add=True)  # дата и время создания
